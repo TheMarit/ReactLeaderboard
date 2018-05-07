e0ca7584cbb41f49bffe19e7ae697881c810b365
Marit <maritoliemans@hotmail.com>
2018-05-07 17:40:00 +0000

    Add sorting logic

--- src/Components/Camper.js
+++ src/Components/Camper.js
@@ -4,14 +4,15 @@ import './Camper.css'
 class Camper extends Component {
     
     render(){
+        let {img, username, recent, alltime} = this.props.camper;
         return (
-        <tr>
-            <td>{this.props.index + 1}</td>
-            <td>{this.props.camper.username}</td>
-            <td className='points'>{this.props.camper.recent}</td>
-            <td className='points'>{this.props.camper.alltime}</td>
+        <tr style={ this.props.index % 2 === 0 ? {backgroundColor: '#edf3ff'} : {backgroundColor: '#D2DFF4'}}>
+            <td className='numbers'>{this.props.index + 1}</td>
+            <td className="CamperName"><img src={img} alt={username}/>{username}</td>
+            <td className='numbers'>{recent}</td>
+            <td className='numbers'>{alltime}</td>
         </tr>
-        )
+        );
     }
 }
 
--- src/Components/Leaderboard.js
+++ src/Components/Leaderboard.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
-import Camper from './Camper'
-import './Leaderboard.css'
+import Camper from './Camper';
+import './Leaderboard.css';
 
 class Leaderboard extends Component {
     constructor(props){
         super(props);
-        this.state = {campers: []};
+        this.state = {campers: [], alltime: false};
+        this.alltime = this.alltime.bind(this);
+        this.recent = this.recent.bind(this);
     }
     
     componentDidMount(){
+        this.recent();
+    }
+    
+    alltime(){
+        fetch('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
+        .then(resp => resp.json())
+        .then(campers => {
+            this.setState({campers, alltime: true});
+        });
+    }
+    recent(){
         fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
         .then(resp => resp.json())
         .then(campers => {
-            this.setState({campers})
-        })
+            this.setState({campers, alltime: false});
+        });
     }
-    
     render(){
+        let thRecent = this.state.alltime ? <th><a onClick={this.recent}>Points in past 30 days</a></th> : <th>Points in past 30 days <span>▼</span></th>;
+        let thAlltime = this.state.alltime ? <th>Alltime Points <span>▼</span></th> : <th><a onClick={this.alltime}>Alltime Points</a></th>;
+        
         let campers = "Loading...";
         if(this.state.campers && this.state.campers.length > 0){
             campers = this.state.campers.map((camper, i) => < Camper key={i} index={i} camper={camper}/> );
         }
-        
-        
         
         return <div className="leaderBoard">
             <div className="header">
@@ -33,12 +46,12 @@ class Leaderboard extends Component {
                 <tr>
                     <th>#</th>
                     <th>Camper Name</th>
-                    <th>Points in past 30 days</th>
-                    <th>Alltime Points</th>
+                    {thRecent}
+                    {thAlltime}
                 </tr>
                 {campers}
             </table>
-        </div>
+        </div>;
     }
 }
 
